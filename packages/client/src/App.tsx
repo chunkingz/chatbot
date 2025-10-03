6bd6be7956d20e41655eee777db50958b01129bf
chunkingz <fortuneking33@yahoo.com>
2025-10-02 23:35:10 -0500

    Setup Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
-  const [msg, setMsg] = useState("")
+   const [msg, setMsg] = useState('');
 
-  useEffect(() => {
-    fetch("/api/v1/hello")
-      .then(res => res.json())
-      .then(data => setMsg(data.message))
-  }, []);
+   useEffect(() => {
+      fetch('/api/v1/hello')
+         .then((res) => res.json())
+         .then((data) => setMsg(data.message));
+   }, []);
 
-  return (
-    <div className='p-4'>
-      <p className='font-bold text-3xl'>{msg}</p>
-      <Button>Click me</Button>
-    </div>
-  )
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{msg}</p>
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,13 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Homepage');
+   res.send('Homepage');
 });
 
 app.get('/api/v1/hello', (req: Request, res: Response) => {
-  res.json({message:'Welcome to the AI Chatbot'});
+   res.json({ message: 'Welcome to the AI Chatbot' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
