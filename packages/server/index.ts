cb5f55dec4a37da02439c13cd2d73e53b1d52b55
chunkingz <fortuneking33@yahoo.com>
2025-10-09 00:37:53 -0500

    Add input validation with zod and Add error handling

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -15,6 +16,15 @@ const PORT = process.env.PORT || 3000;
 
 const conversations = new Map<string, string>();
 
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt is too long (max 1000 characters)'),
+   conversationId: z.uuid(),
+});
+
 app.get('/', (req: Request, res: Response) => {
    res.send('Homepage');
 });
@@ -24,18 +34,26 @@ app.get('/api/v1/hello', (req: Request, res: Response) => {
 });
 
 app.post('/api/v1/chat', async (req: Request, res: Response) => {
-   const { prompt, conversationId } = req.body;
-   const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id: conversations.get(conversationId),
-   });
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      return res.status(400).json(parseResult.error);
+   }
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini-',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id: conversations.get(conversationId),
+      });
 
-   conversations.set(conversationId, response.id);
+      conversations.set(conversationId, response.id);
 
-   res.json({ message: response.output_text });
+      res.json({ message: response.output_text });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to generate a response.' });
+   }
 });
 
 app.listen(PORT, () => {
