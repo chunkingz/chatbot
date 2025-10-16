e69d33e84b072bd86c137769501de3098a001ad6
chunkingz <fortuneking33@yahoo.com>
2025-10-16 06:06:57 -0500

    Build and style the chat bot component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from './components/ui/button';
+import Chatbot from './components/Chatbot';
 
 function App() {
-   const [msg, setMsg] = useState('');
-
-   useEffect(() => {
-      fetch('/api/v1/hello')
-         .then((res) => res.json())
-         .then((data) => setMsg(data.message));
-   }, []);
-
    return (
       <div className="p-4">
-         <p className="font-bold text-3xl">{msg}</p>
-         <Button>Click me</Button>
+         <Chatbot />
       </div>
    );
 }
